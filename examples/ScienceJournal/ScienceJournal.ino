ac6ba5db801cb956bf19b25cf7b845d429b17937
Giovanni Bruno <g.bruno@arduino.cc>
2023-06-05 13:10:03 +0200

    keywords, properties

--- examples/ScienceJournal/ScienceJournal.ino
+++ examples/ScienceJournal/ScienceJournal.ino
@@ -1,3 +1,22 @@
+/*
+  This file is part of the Arduino_GroveI2C_Ultrasonic library.
+  Copyright (c) 2023 Arduino SA. All rights reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 #include "ble_config.h"
 #include "Arduino_ScienceKitCarrier.h"
 String name;
@@ -56,7 +75,7 @@ void setup() {
 
   BLE.addService(service);
   BLE.advertise();
-  science_kit.startAuxiliaryThreads(); // start the bme688 thread
+  science_kit.startAuxiliaryThreads(); // start the BME688 thread
   _t.start(loop_data);
   _tu.start(update);
 }
